9e807014ee08fee37ef0dd15f4c40bfe816f24a9
liebeslenchen <lr878249@fh-muenster.de>
2013-05-22 13:51:49 +0200

    Added Activity

---
+++ Ludo/src/de/ludo/android/MainActivity.java
@@ -0,0 +1,18 @@
+package de.ludo.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
+
+}
